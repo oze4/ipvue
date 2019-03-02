dcf2ff43bde2a832e27723c2c1e0da8620724fab
oze4 <matthewpoestreich@gmail.com>
2019-03-02 13:53:39 -0600

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
+const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin([{
+        /**
+         * This is needed for GitHub Pages to work correctly it copies
+         * the CNAME file (at the root of project) & necessry images
+         * into the /docs directory after a rebuild.
+         */
+        from: 'CNAME',
+        to: 'CNAME',
+        toType: 'file',
+      }])
+    ],
+  },
   publicPath: '',
   outputDir: './docs',
   assetsDir: 'assets',
