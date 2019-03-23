05b3994b0e7ca8e137518cd396a9d619369f297e
oze4 <matthewpoestreich@gmail.com>
2019-03-23 17:01:01 -0500

    update

---
+++ docs/precache-manifest.5e8c2d292f42a8dc7ef6949bc454e83e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "579f83320af5019f5447",
+    "url": "assets/css/chunk-e43a9280.daa8cfe1.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "579f83320af5019f5447",
+    "url": "assets/js/chunk-e43a9280.ebe78f04.js"
+  },
+  {
+    "revision": "1f7c96a1e1678dd5fbbf",
+    "url": "assets/js/chunk-4feae546.8ee2b1d8.js"
+  },
+  {
+    "revision": "e2ba324da54f36a6ad9b",
+    "url": "assets/js/chunk-74983c22.946ef2c0.js"
+  },
+  {
+    "revision": "9b9edfb0deb12e2d7a48d255b7e83a1e",
+    "url": "index.html"
+  },
+  {
+    "revision": "4be98e89a29a758a6854",
+    "url": "assets/js/chunk-792adf16.dd185489.js"
+  },
+  {
+    "revision": "6252d96c7790ffd49329",
+    "url": "assets/js/chunk-vendors.007949ed.js"
+  },
+  {
+    "revision": "f7eaa8e827bb431dba99",
+    "url": "assets/js/chunk-803a7890.1db2c71f.js"
+  },
+  {
+    "revision": "d540fee8d6cb3fe2ed5c",
+    "url": "assets/js/chunk-f7bbe3fe.fc23da6a.js"
+  },
+  {
+    "revision": "18a03df502959f6da0aa",
+    "url": "assets/js/app.05b3d6e9.js"
+  },
+  {
+    "revision": "cf23526f451784ff137f161b8fe18d5a",
+    "url": "assets/img/vue.png"
+  },
+  {
+    "revision": "8e0c2e0571368a5cce92e3432b55fd1c",
+    "url": "assets/img/ostrike_logo.png"
+  },
+  {
+    "revision": "6252d96c7790ffd49329",
+    "url": "assets/css/chunk-vendors.4d9ce6f5.css"
+  },
+  {
+    "revision": "d540fee8d6cb3fe2ed5c",
+    "url": "assets/css/chunk-f7bbe3fe.a283228c.css"
+  },
+  {
+    "revision": "f7eaa8e827bb431dba99",
+    "url": "assets/css/chunk-803a7890.cddd8466.css"
+  },
+  {
+    "revision": "4be98e89a29a758a6854",
+    "url": "assets/css/chunk-792adf16.6767c9d8.css"
+  },
+  {
+    "revision": "e2ba324da54f36a6ad9b",
+    "url": "assets/css/chunk-74983c22.1009418a.css"
+  },
+  {
+    "revision": "1f7c96a1e1678dd5fbbf",
+    "url": "assets/css/chunk-4feae546.36cdfe4b.css"
+  }
+];
