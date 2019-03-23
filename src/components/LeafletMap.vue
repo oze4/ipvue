707269c519b992f9cff2d891ea79ace45c664fb9
oze4 <matthewpoestreich@gmail.com>
2019-03-23 18:27:23 -0500

    fixed re-rendering issue in v-for leaflet-map

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -2,7 +2,7 @@
     <div>
         <v-layout row wrap justify-center mb-5>
             <v-flex xs10 sm10 md10 lg10>
-                <v-card elevation="24" class='leaflet-map-container'>
+                <v-card elevation="24" style="overflow-x: auto;">
                     <v-toolbar color="primary lighten-1" dark flat>
                         <v-spacer></v-spacer>
                         <v-toolbar-title>
@@ -121,9 +121,3 @@ export default {
     },
 }
 </script>
-
-<style scoped>
-.leaflet-map-container {
-    overflow-x: auto;
-}
-</style>
