ce7c2706c494e6a9c0ee356635e98cd6036f5315
Matt Oestreich <matthewpoestreich@gmail.com>
2019-04-13 01:43:30 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,6 @@ module.exports = {
     entry: ['babel-polyfill', './src/main.js'],
     plugins: [
       new CopyPlugin([{
-        /**
-         * This is needed for GitHub Pages to work correctly it copies
-         * the CNAME file (at the root of project) into the /docs directory after a rebuild.
-         */
         from: 'CNAME',
         to: 'CNAME',
         toType: 'file',
